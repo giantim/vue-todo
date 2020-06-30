33506452b35bd1efd578e90aee213a259a43046e
giantim <giantim132@gmail.com>
2020-06-30 14:23:27 +0900

    refactor: 비어있는 할 일을 입력할 수 없도록 변경

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -22,6 +22,10 @@
 
         methods: {
             addTodo(event) {
+                if (event.target.value === '') {
+                    alert('비어있는 할 일을 입력할 수 없습니다.');
+                    return;
+                }
                 this.$store.commit('ADD_TODO', event.target.value);
                 this.content = '';
             }
